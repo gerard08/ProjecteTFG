bb64d15423881e2b7cd6aa48ab991b40fb6b7960
gerard_08 <gerardmaresp@gmail.com>
2021-11-06 18:29:27 +0100

    Codi actualitzat per descarregar imatge i relleu

--- codi/ProvaMesSeria.py
+++ codi/ProvaMesSeria.py
@@ -1,11 +1,15 @@
 import icgc
-xy0 = (40.76, 0.58)
-xy1 = (40.96, 0.78)
+xy0 = (40.77, 0.37)
+xy1 = (40.87, 0.47)
 
 (xy0,xy1) = icgc.calculateCoord(xy0, xy1)
 
 
-im = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 2080, 1080)
+im = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 1080, 1080)
+imr = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 1080, 1080, 'relleu')
 file = open('rebut.png', 'wb')
 file.write(im)
+file.close()
+file = open('relleu.png', 'wb')
+file.write(imr)
 file.close()
