d1de2837c1a92be62b52840d9626c3a7f8689783
gerard_08 <gerardmaresp@gmail.com>
2021-11-07 12:39:51 +0100

    descarrega imatges (no acabat)

--- codi/ProvaMesSeria.py
+++ codi/ProvaMesSeria.py
@@ -1,12 +1,12 @@
 import icgc
 xy0 = (40.77, 0.37)
-xy1 = (40.87, 0.47)
+xy1 = (40.78, 0.38)
 
 (xy0,xy1) = icgc.calculateCoord(xy0, xy1)
 
 
-im = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 1080, 1080)
-imr = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 1080, 1080, 'relleu')
+im = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 572, 572)
+imr = icgc.getImage(xy0[0], xy0[1], xy1[0], xy1[1], 572, 572, 'relleu')
 file = open('rebut.png', 'wb')
 file.write(im)
 file.close()
